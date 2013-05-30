a9a27095e6f18599a0ab3347ed6f7207965f4113
Dfkuro <izmirreffi@gmail.com>
2013-05-30 18:18:51 -0500

    changes 001
    mod change 001

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
+
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
     <!-- Le styles -->
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/new_style.css" rel="stylesheet">
@@ -20,10 +24,414 @@
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="favicon.ico">
   </head>
-
+	
   <body>	
   	<div class="container">
-		h1{content } 
+
+  		<div class="navbar navbar-fixed-top">
+  		      <div class="navbar-inner">
+  		        <div class="container">
+  		        	
+  		          <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+  		          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+  		            <span class="icon-bar"></span>
+  		            <span class="icon-bar"></span>
+  		            <span class="icon-bar"></span>
+  		          </a>
+  		
+  		          <!-- Be sure to leave the brand out there if you want it shown -->
+  		          <a class="brand" href="#">Project name</a>
+  		
+  		          <!-- Everything you want hidden at 940px or less, place within here -->
+  		          <div class="nav-collapse collapse">
+  		
+  		          	<!-- .nav, .navbar-search, .navbar-form, etc -->
+  		            <ul class="nav">
+  		              <li class="active"><a href="#">Home</a></li>
+  		              <li><a href="#about">About</a></li>
+  		              <li><a href="#contact">Contact</a></li>
+  		            </ul>
+  		
+  		            <form class="navbar-form pull-right">
+  		              <input class="span2" type="text" placeholder="Email">
+  		              <input class="span2" type="password" placeholder="Password">
+  		              <button type="submit" class="btn">Sign in</button>
+  		            </form>
+  		            
+  		          </div>
+  		        </div>
+  		      </div>
+  		    </div>
+		
+		<script type="text/javascript">
+			$(document).ready(function() {
+				//alert('hello');
+			});
+		</script>
+
+		<div class="row">
+			<div class="span12">
+				
+				
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="span6">Column 1</div>
+			<div class="span6"><ul>
+				<li>1.-akdasd</li>
+				<li>2.-akdasd</li>
+				<li>3.-akdasd</li>
+				<li>4.-akdasd</li>
+				<li>5.-akdasd</li>
+				<li>6.-akdasd</li>
+				<li>7.-akdasd</li>
+				<li>8.-akdasd</li>
+				<li>9.-akdasd</li>
+				<li>10.-akdasd</li>
+				<li>11.-akdasd</li>
+				<li>12.-akdasd</li>
+				<li>13.-akdasd</li>
+				<li>14.-akdasd</li>
+				<li>15.-akdasd</li>
+				<li>16.-akdasd</li>
+				<li>17.-akdasd</li>
+				<li>18.-akdasd</li>
+				<li>19.-akdasd</li>
+				<li>20.-akdasd</li>
+				<li>21.-akdasd</li>
+				<li>22.-akdasd</li>
+				<li>23.-akdasd</li>
+				<li>24.-akdasd</li>
+				<li>25.-akdasd</li>
+				<li>26.-akdasd</li>
+				<li>27.-akdasd</li>
+				<li>28.-akdasd</li>
+				<li>29.-akdasd</li>
+				<li>30.-akdasd</li>
+				<li>31.-akdasd</li>
+				<li>32.-akdasd</li>
+				<li>33.-akdasd</li>
+				<li>34.-akdasd</li>
+				<li>35.-akdasd</li>
+				<li>36.-akdasd</li>
+				<li>37.-akdasd</li>
+				<li>38.-akdasd</li>
+				<li>39.-akdasd</li>
+				<li>40.-akdasd</li>
+				<li>41.-akdasd</li>
+				<li>42.-akdasd</li>
+				<li>43.-akdasd</li>
+				<li>44.-akdasd</li>
+				<li>45.-akdasd</li>
+				<li>46.-akdasd</li>
+				<li>47.-akdasd</li>
+				<li>48.-akdasd</li>
+				<li>49.-akdasd</li>
+				<li>50.-akdasd</li>
+				<li>51.-akdasd</li>
+				<li>52.-akdasd</li>
+				<li>53.-akdasd</li>
+				<li>54.-akdasd</li>
+				<li>55.-akdasd</li>
+				<li>56.-akdasd</li>
+				<li>57.-akdasd</li>
+				<li>58.-akdasd</li>
+				<li>59.-akdasd</li>
+				<li>60.-akdasd</li>
+				<li>61.-akdasd</li>
+				<li>62.-akdasd</li>
+				<li>63.-akdasd</li>
+				<li>64.-akdasd</li>
+				<li>65.-akdasd</li>
+				<li>66.-akdasd</li>
+				<li>67.-akdasd</li>
+				<li>68.-akdasd</li>
+				<li>69.-akdasd</li>
+				<li>70.-akdasd</li>
+				<li>71.-akdasd</li>
+				<li>72.-akdasd</li>
+				<li>73.-akdasd</li>
+				<li>74.-akdasd</li>
+				<li>75.-akdasd</li>
+				<li>76.-akdasd</li>
+				<li>77.-akdasd</li>
+				<li>78.-akdasd</li>
+				<li>79.-akdasd</li>
+				<li>80.-akdasd</li>
+				<li>81.-akdasd</li>
+				<li>82.-akdasd</li>
+				<li>83.-akdasd</li>
+				<li>84.-akdasd</li>
+				<li>85.-akdasd</li>
+				<li>86.-akdasd</li>
+				<li>87.-akdasd</li>
+				<li>88.-akdasd</li>
+				<li>89.-akdasd</li>
+				<li>90.-akdasd</li>
+				<li>91.-akdasd</li>
+				<li>92.-akdasd</li>
+				<li>93.-akdasd</li>
+				<li>94.-akdasd</li>
+				<li>95.-akdasd</li>
+				<li>96.-akdasd</li>
+				<li>97.-akdasd</li>
+				<li>98.-akdasd</li>
+				<li>99.-akdasd</li>
+				<li>100.-akdasd</li>
+				<li>101.-akdasd</li>
+				<li>102.-akdasd</li>
+				<li>103.-akdasd</li>
+				<li>104.-akdasd</li>
+				<li>105.-akdasd</li>
+				<li>106.-akdasd</li>
+				<li>107.-akdasd</li>
+				<li>108.-akdasd</li>
+				<li>109.-akdasd</li>
+				<li>110.-akdasd</li>
+				<li>111.-akdasd</li>
+				<li>112.-akdasd</li>
+				<li>113.-akdasd</li>
+				<li>114.-akdasd</li>
+				<li>115.-akdasd</li>
+				<li>116.-akdasd</li>
+				<li>117.-akdasd</li>
+				<li>118.-akdasd</li>
+				<li>119.-akdasd</li>
+				<li>120.-akdasd</li>
+				<li>121.-akdasd</li>
+				<li>122.-akdasd</li>
+				<li>123.-akdasd</li>
+				<li>124.-akdasd</li>
+				<li>125.-akdasd</li>
+				<li>126.-akdasd</li>
+				<li>127.-akdasd</li>
+				<li>128.-akdasd</li>
+				<li>129.-akdasd</li>
+				<li>130.-akdasd</li>
+				<li>131.-akdasd</li>
+				<li>132.-akdasd</li>
+				<li>133.-akdasd</li>
+				<li>134.-akdasd</li>
+				<li>135.-akdasd</li>
+				<li>136.-akdasd</li>
+				<li>137.-akdasd</li>
+				<li>138.-akdasd</li>
+				<li>139.-akdasd</li>
+				<li>140.-akdasd</li>
+				<li>141.-akdasd</li>
+				<li>142.-akdasd</li>
+				<li>143.-akdasd</li>
+				<li>144.-akdasd</li>
+				<li>145.-akdasd</li>
+				<li>146.-akdasd</li>
+				<li>147.-akdasd</li>
+				<li>148.-akdasd</li>
+				<li>149.-akdasd</li>
+				<li>150.-akdasd</li>
+				<li>151.-akdasd</li>
+				<li>152.-akdasd</li>
+				<li>153.-akdasd</li>
+				<li>154.-akdasd</li>
+				<li>155.-akdasd</li>
+				<li>156.-akdasd</li>
+				<li>157.-akdasd</li>
+				<li>158.-akdasd</li>
+				<li>159.-akdasd</li>
+				<li>160.-akdasd</li>
+				<li>161.-akdasd</li>
+				<li>162.-akdasd</li>
+				<li>163.-akdasd</li>
+				<li>164.-akdasd</li>
+				<li>165.-akdasd</li>
+				<li>166.-akdasd</li>
+				<li>167.-akdasd</li>
+				<li>168.-akdasd</li>
+				<li>169.-akdasd</li>
+				<li>170.-akdasd</li>
+				<li>171.-akdasd</li>
+				<li>172.-akdasd</li>
+				<li>173.-akdasd</li>
+				<li>174.-akdasd</li>
+				<li>175.-akdasd</li>
+				<li>176.-akdasd</li>
+				<li>177.-akdasd</li>
+				<li>178.-akdasd</li>
+				<li>179.-akdasd</li>
+				<li>180.-akdasd</li>
+				<li>181.-akdasd</li>
+				<li>182.-akdasd</li>
+				<li>183.-akdasd</li>
+				<li>184.-akdasd</li>
+				<li>185.-akdasd</li>
+				<li>186.-akdasd</li>
+				<li>187.-akdasd</li>
+				<li>188.-akdasd</li>
+				<li>189.-akdasd</li>
+				<li>190.-akdasd</li>
+				<li>191.-akdasd</li>
+				<li>192.-akdasd</li>
+				<li>193.-akdasd</li>
+				<li>194.-akdasd</li>
+				<li>195.-akdasd</li>
+				<li>196.-akdasd</li>
+				<li>197.-akdasd</li>
+				<li>198.-akdasd</li>
+				<li>199.-akdasd</li>
+				<li>200.-akdasd</li>
+				<li>201.-akdasd</li>
+				<li>202.-akdasd</li>
+				<li>203.-akdasd</li>
+				<li>204.-akdasd</li>
+				<li>205.-akdasd</li>
+				<li>206.-akdasd</li>
+				<li>207.-akdasd</li>
+				<li>208.-akdasd</li>
+				<li>209.-akdasd</li>
+				<li>210.-akdasd</li>
+				<li>211.-akdasd</li>
+				<li>212.-akdasd</li>
+				<li>213.-akdasd</li>
+				<li>214.-akdasd</li>
+				<li>215.-akdasd</li>
+				<li>216.-akdasd</li>
+				<li>217.-akdasd</li>
+				<li>218.-akdasd</li>
+				<li>219.-akdasd</li>
+				<li>220.-akdasd</li>
+				<li>221.-akdasd</li>
+				<li>222.-akdasd</li>
+				<li>223.-akdasd</li>
+				<li>224.-akdasd</li>
+				<li>225.-akdasd</li>
+				<li>226.-akdasd</li>
+				<li>227.-akdasd</li>
+				<li>228.-akdasd</li>
+				<li>229.-akdasd</li>
+				<li>230.-akdasd</li>
+				<li>231.-akdasd</li>
+				<li>232.-akdasd</li>
+				<li>233.-akdasd</li>
+				<li>234.-akdasd</li>
+				<li>235.-akdasd</li>
+				<li>236.-akdasd</li>
+				<li>237.-akdasd</li>
+				<li>238.-akdasd</li>
+				<li>239.-akdasd</li>
+				<li>240.-akdasd</li>
+				<li>241.-akdasd</li>
+				<li>242.-akdasd</li>
+				<li>243.-akdasd</li>
+				<li>244.-akdasd</li>
+				<li>245.-akdasd</li>
+				<li>246.-akdasd</li>
+				<li>247.-akdasd</li>
+				<li>248.-akdasd</li>
+				<li>249.-akdasd</li>
+				<li>250.-akdasd</li>
+				<li>251.-akdasd</li>
+				<li>252.-akdasd</li>
+				<li>253.-akdasd</li>
+				<li>254.-akdasd</li>
+				<li>255.-akdasd</li>
+				<li>256.-akdasd</li>
+				<li>257.-akdasd</li>
+				<li>258.-akdasd</li>
+				<li>259.-akdasd</li>
+				<li>260.-akdasd</li>
+				<li>261.-akdasd</li>
+				<li>262.-akdasd</li>
+				<li>263.-akdasd</li>
+				<li>264.-akdasd</li>
+				<li>265.-akdasd</li>
+				<li>266.-akdasd</li>
+				<li>267.-akdasd</li>
+				<li>268.-akdasd</li>
+				<li>269.-akdasd</li>
+				<li>270.-akdasd</li>
+				<li>271.-akdasd</li>
+				<li>272.-akdasd</li>
+				<li>273.-akdasd</li>
+				<li>274.-akdasd</li>
+				<li>275.-akdasd</li>
+				<li>276.-akdasd</li>
+				<li>277.-akdasd</li>
+				<li>278.-akdasd</li>
+				<li>279.-akdasd</li>
+				<li>280.-akdasd</li>
+				<li>281.-akdasd</li>
+				<li>282.-akdasd</li>
+				<li>283.-akdasd</li>
+				<li>284.-akdasd</li>
+				<li>285.-akdasd</li>
+				<li>286.-akdasd</li>
+				<li>287.-akdasd</li>
+				<li>288.-akdasd</li>
+				<li>289.-akdasd</li>
+				<li>290.-akdasd</li>
+				<li>291.-akdasd</li>
+				<li>292.-akdasd</li>
+				<li>293.-akdasd</li>
+				<li>294.-akdasd</li>
+				<li>295.-akdasd</li>
+				<li>296.-akdasd</li>
+				<li>297.-akdasd</li>
+				<li>298.-akdasd</li>
+				<li>299.-akdasd</li>
+				<li>300.-akdasd</li>
+				<li>301.-akdasd</li>
+				<li>302.-akdasd</li>
+				<li>303.-akdasd</li>
+				<li>304.-akdasd</li>
+				<li>305.-akdasd</li>
+				<li>306.-akdasd</li>
+				<li>307.-akdasd</li>
+				<li>308.-akdasd</li>
+				<li>309.-akdasd</li>
+				<li>310.-akdasd</li>
+				<li>311.-akdasd</li>
+				<li>312.-akdasd</li>
+				<li>313.-akdasd</li>
+				<li>314.-akdasd</li>
+				<li>315.-akdasd</li>
+				<li>316.-akdasd</li>
+				<li>317.-akdasd</li>
+				<li>318.-akdasd</li>
+				<li>319.-akdasd</li>
+				<li>320.-akdasd</li>
+				<li>321.-akdasd</li>
+				<li>322.-akdasd</li>
+				<li>323.-akdasd</li>
+				<li>324.-akdasd</li>
+				<li>325.-akdasd</li>
+				<li>326.-akdasd</li>
+				<li>327.-akdasd</li>
+				<li>328.-akdasd</li>
+				<li>329.-akdasd</li>
+				<li>330.-akdasd</li>
+				<li>331.-akdasd</li>
+				<li>332.-akdasd</li>
+				<li>333.-akdasd</li>
+				<li>334.-akdasd</li>
+				<li>335.-akdasd</li>
+				<li>336.-akdasd</li>
+				<li>337.-akdasd</li>
+				<li>338.-akdasd</li>
+				<li>339.-akdasd</li>
+				<li>340.-akdasd</li>
+				<li>341.-akdasd</li>
+				<li>342.-akdasd</li>
+				<li>343.-akdasd</li>
+				<li>344.-akdasd</li>
+				<li>345.-akdasd</li>
+				<li>346.-akdasd</li>
+				<li>347.-akdasd</li>
+				<li>348.-akdasd</li>
+				<li>349.-akdasd</li>
+				<li>350.-akdasd</li>
+			</ul></div>
+		</div>
+
   	</div>
 	
 
@@ -31,7 +439,7 @@
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.1/jquery.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
   </body>
